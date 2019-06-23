8f6a2b5d64a9335102ff98156f1facf8f9300926
Sam Symons <sam@samsymons.com>
2019-06-24 09:38:00 +1200

    Update Gulp and other dependencies.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,48 +1,41 @@
-var gulp         = require("gulp"),
-    sass         = require("gulp-sass"),
-    autoprefixer = require("gulp-autoprefixer")
-    hash         = require("gulp-hash"),
-    del          = require("del")
+const gulp = require("gulp");
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sass = require("gulp-sass");
+const hash = require("gulp-hash");
+const del = require("del");
 
-// Process SCSS
+function clean() {
+  return del(["static/css/**/*", "static/js/**/*", "static/images/**/*"])
+}
 
-gulp.task("scss", function() {
-  del(["static/css/**/*"])
+function css() {
+  return gulp.src("assets/scss/**/*.scss")
+    .pipe(sass({
+      outputStyle: "compressed"
+    }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(hash())
+    .pipe(gulp.dest("static/css"))
+    .pipe(hash.manifest("hash.json"))
+    .pipe(gulp.dest("data/css"))
+}
 
-  gulp.src("assets/scss/**/*.scss")
-      .pipe(sass({outputStyle: "compressed"}))
-      .pipe(autoprefixer({browsers: ["last 20 versions"]}))
-      .pipe(hash())
-      .pipe(gulp.dest("static/css"))
-      .pipe(hash.manifest("hash.json"))
-      .pipe(gulp.dest("data/css"))
-})
-
-// Process Images
-
-gulp.task("images", function () {
-  del(["static/images/**/*"])
-
-  gulp.src("assets/images/**/*")
+function images() {
+    return gulp.src("assets/images/**/*")
       .pipe(gulp.dest("static/images"))
-})
+}
 
 // Process JavaScript
 
-gulp.task("js", function () {
-  del(["static/js/**/*"])
+function js() {
+  return gulp.src("assets/js/**/*")
+    .pipe(hash())
+    .pipe(gulp.dest("static/js"))
+    .pipe(hash.manifest("hash.json"))
+    .pipe(gulp.dest("data/js"))
+}
 
-  gulp.src("assets/js/**/*")
-      .pipe(hash())
-      .pipe(gulp.dest("static/js"))
-      .pipe(hash.manifest("hash.json"))
-      .pipe(gulp.dest("data/js"))
-})
-
-gulp.task("watch", ["scss", "images", "js"], function () {
-  gulp.watch("assets/scss/**/*", ["scss"])
-  gulp.watch("assets/images/**/*", ["images"])
-  gulp.watch("assets/js/**/*", ["js"])
-})
-
-gulp.task("default", ["watch"])
+const build = gulp.series(clean, gulp.parallel(css, images, js));
+exports.build = build;
+exports.default = build;
